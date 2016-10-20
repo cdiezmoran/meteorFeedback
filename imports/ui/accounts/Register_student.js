11a03f95b0c2ad62466a346473dfe19851cd7aa4
Carlos Diez <cdiezmoran@gmail.com>
2016-10-20 17:45:20 -0500

    Added some logic to student register file

--- imports/ui/accounts/Register_student.js
+++ imports/ui/accounts/Register_student.js
@@ -2,12 +2,13 @@ import './Register_student.html';
 
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 //import partials
 import '../partials/Student_join.html';
 
 Template.Register_student.onCreated(() => {
-
+  BlazeLayout.render('Register_student', { content: 'Student_join' });
 });
 
 Template.Register_student.helpers({
@@ -16,6 +17,13 @@ Template.Register_student.helpers({
 
 Template.Register_student.events({
   'click #code-submit': (event) => {
-
+    //if classcode exists render signup template
+    BlazeLayout.render('Register_student', { content: 'Student_signup' });
+    //else throw error
+  },
+  'click #signup-submit': (event) => {
+    //get values from textfields
+    //create a new user
+    Accounts.create()
   }
 });
