691e2a5878f6ae2f2a38c8b1b37314d70e7269f5
Carlos Diez <cdiezmoran@gmail.com>
2016-10-20 17:08:56 -0500

    Templates ready

---
+++ imports/ui/accounts/Register_student.js
@@ -0,0 +1,21 @@
+import './Register_student.html';
+
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+//import partials
+import '../partials/Student_join.html';
+
+Template.Register_student.onCreated(() => {
+
+});
+
+Template.Register_student.helpers({
+
+});
+
+Template.Register_student.events({
+  'click #code-submit': (event) => {
+
+  }
+});
